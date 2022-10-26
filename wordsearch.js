921cdba62227e631a11aad41930a2105d924e7a4
Andy Farquharson <andyfarquharson@gmail.com>
2022-10-26 22:49:15 +0000

    Pair programming wordsearch

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,24 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  if (!word) {
+    return false;
+  }
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
 
-module.exports = wordSearch
+  let copyMatrix = [];
+  for (let i = 0; i < letters[0].length; i++) {
+    copyMatrix[i] = [];
+    for (let j = 0; j < letters.length; j++) {
+      copyMatrix[i][j] = letters[j][i];
+    }
+  }
+  const verticalJoin = copyMatrix.map(ls => ls.join(''));
+  for (const l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
+
+module.exports = wordSearch;
